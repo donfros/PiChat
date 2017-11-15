0a1f5b45a0dfdbf68ff9a92c795c1c344da052e3
Steven D'Onfro <donfros@wit.edu>
2017-11-15 13:43:52 -0500

    Killin the game

--- PiChat - Network Programming Final/src/PiChat/Client.java
+++ PiChat - Network Programming Final/src/PiChat/Client.java
@@ -13,16 +13,15 @@ public class Client implements Runnable {
 	private static Socket client = null;
 	private static DataInputStream is = null;
 	private static BufferedReader iLine = null;
-	private static BufferedReader inStream = null;
 	private static DataOutputStream os = null;
 	private static boolean closed = false;
 
 	public static void main(String[] args) {
 
+		// Open socket and initialize in/out streams
 		try {
 			client = new Socket("1.2.3.4", PORT);
 			is = new DataInputStream(client.getInputStream());
-			inStream = new BufferedReader(new InputStreamReader(System.in));
 			os = new DataOutputStream(client.getOutputStream());
 			iLine = new BufferedReader(new InputStreamReader(System.in));
 
@@ -30,24 +29,31 @@ public class Client implements Runnable {
 			System.out.println(e);
 		}
 
-		try {
-			os.close();
-			is.close();
-			client.close();
-		} catch (IOException e) {
-			System.out.println(e);
+		// write to the socket
+		if (client != null && is != null && os != null) {
+			try {
+				new Thread(new Client()).start();
+				while (!closed) {
+					os.writeUTF(iLine.readLine().trim());
+				}
+				os.close();
+				is.close();
+				client.close();
+			} catch (IOException e) {
+				System.out.println(e);
+			}
 		}
 	}
 
 	/**
-	 * Thread reads from sever. (non-Javadoc)
+	 * Thread reads from server. (non-Javadoc)
 	 * 
 	 * @see java.lang.Runnable#run()
 	 */
 	public void run() {
 		String response;
 		try {
-			while ((response = inStream.readLine()) != null) {
+			while ((response = is.readLine()) != null) {
 				System.out.println(response);
 				if (response.equals("Ending")) {
 					break;
