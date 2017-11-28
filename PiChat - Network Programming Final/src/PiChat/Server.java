0f2e09450b4310294dccd95819c68964d06b62dc
Evan J. Goyuk <goyuke@wit.edu>
2017-11-28 10:30:52 -0500

    java docs

--- PiChat - Network Programming Final/src/PiChat/Server.java
+++ PiChat - Network Programming Final/src/PiChat/Server.java
@@ -50,8 +50,9 @@ public class Server {
 	}
 
 	/**
-	 * 
+	 * Allows for multi-threading for multiple users to join a server and talk to eachother in realtime
 	 * @author Evan Goyuk
+	 * 
 	 *
 	 */
 	public static class clientThread extends Thread {
@@ -65,8 +66,10 @@ public class Server {
 		
 		/**default constructor 
 		 * 
-		 * @param client
+		 * @param client 
+		 * 		socket of client currently at
 		 * @param threads
+		 *      array of spots for clients to fill
 		 */
 
 		public clientThread(Socket client, clientThread[] threads) {
@@ -87,7 +90,7 @@ public class Server {
 
 			try {
 
-				// Makees input/output streams for each client
+				// Makes input/output streams for each client
 
 				in = new DataInputStream(cSock.getInputStream());
 				out = new PrintStream(cSock.getOutputStream());
